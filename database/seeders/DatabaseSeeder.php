2973767a3e0b516e0e9725a67c7703764ea07816
hemery <fred.hemery@univ-artois.fr>
2020-12-12 20:29:48 +0100

    modif seeder : achats différents pour un jeu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Mecanique;
 use App\Models\User;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder {
     /**
@@ -16,6 +17,13 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
+        User::factory()->create([
+            'name' => 'Robert Duchmol',
+            'email' => 'robert.duchmol@domain.fr',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$TKaVnYUC6y/IPQk8Gjaw7uBB.1xqNnqi/n4xo5uBH6Eok6ZrEpQdC', // secret00 je crois
+            'remember_token' => Str::random(10),
+        ]);
         $this->call(
             [
                 EditeursSeederTable::class,
@@ -29,20 +37,22 @@ class DatabaseSeeder extends Seeder {
         $mecanisque_ids = Mecanique::all()->pluck('id');
         $user_ids = User::all()->pluck('id');
         foreach ($jeux as $jeu) {
-            $nbMecs = $faker->numberBetween(1,3);
+            $nbMecs = $faker->numberBetween(1, 3);
             $mecs = $faker->randomElements($mecanisque_ids, $nbMecs);
             $jeu->mecaniques()->attach($mecs);
-            $nbAchats = $faker->numberBetween(2,5);
-            $achats =  $faker->randomElements($user_ids, $nbAchats);
-            $jeu->acheteurs()->attach($achats, ['lieu' => $faker->word(),
-                'prix' => $faker->randomFloat(2, 50, 250),
-                'date_achat' => $faker->dateTimeInInterval(
-                    $startDate = '-6 months',
-                    $interval = '+ 180 days',
-                    $timezone = date_default_timezone_get()
-                )]);
+            $nbAchats = $faker->numberBetween(2, 5);
+            $achat_ids = $faker->randomElements($user_ids, $nbAchats);
+            for ($i = 0; $i < $nbAchats; $i++) {
+                $jeu->acheteurs()->attach($achat_ids[$i], ['lieu' => $faker->word(),
+                    'prix' => $faker->randomFloat(2, 50, 250),
+                    'date_achat' => $faker->dateTimeInInterval(
+                        $startDate = '-6 months',
+                        $interval = '+ 180 days',
+                        $timezone = date_default_timezone_get()
+                    )]);
+            }
             $jeu->save();
-         }
+        }
         Commentaire::factory(100)->create();
     }
 }
