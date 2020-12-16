7ac5ea05b4dc579a31c07c9f84492889aa6be91c
harold delory <harold_delory@ens.univ-artois.fr>
2020-12-16 18:28:48 +0100

    Jalon 2 #30.     v.1 : Ajout des statistiques commentaires d'un jeu. Il manque le classement.

--- resources/views/info-jeu.blade.php
+++ resources/views/info-jeu.blade.php
@@ -1,6 +1,38 @@
 <html lang="fr">
     <body>
         <h1 style="align-items: center"> Détails du jeu </h1>
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    {{$note = 0}}
+                    @foreach($jeu -> $commentaires as $commentaire)
+                        {{$note += $commentaire->note}}
+                    @endforeach
+                    {{$moyenne = $note/\count($jeu->commentaires)}}
+                </div>
+                <div class="col">
+                    {{$jeu->commentaires->max()->note}}
+                </div>
+                <div class="col">
+                    {{$jeu->commentaires->min()->note}}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    {{count($jeu->commentaires)}}
+                </div>
+                <div class="col">
+                    {{\App\Models\Commentaire::all()->count()}}
+                </div>
+                <div class="col">
+                    3 of 3
+                </div>
+            </div>
+        </div>
+
+
+
         <ul>
             <li>
                 <p>  {{$jeu -> url_media}} </p>
@@ -19,7 +51,9 @@
                 <p> <strong> Durée </strong>: {{$jeu -> duree}} </p>
             </li>
         </ul>
+
         <a href="{{route('regle.show', ['id' => $jeu->id])}}"><button>Voir les regles</button></a>
         <a href="{{route('jeux.index')}}"><button>Retour a la liste</button></a>
+
     </body>
 </html>
