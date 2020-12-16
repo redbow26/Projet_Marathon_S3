5526fc6ff514615bce3e085d4a08e13536ed358e
Theo PRAT <theo@linux.home>
2020-12-16 15:47:49 +0100

    Jalon 1-13 seeder début

--- database/seeders/JeuxSeeder.php
+++ database/seeders/JeuxSeeder.php
@@ -6,6 +6,19 @@ use Illuminate\Database\Seeder;
 
 class JeuxSeeder extends Seeder
 {
+
+    const titre = ['Death Note',
+        'Cowboy Bebop',
+        "L'Attaque des Titans",
+        "Fullmetal Alchemist : Brotherhood",
+        "Samurai Champloo",
+        "Neon Genesis Evangelion ",
+        "Hunter x Hunter ",
+        "One Piece",
+        "Code Geass: Lelouch of the Rebellion ",
+        "Dragon Ball Z ("
+        ];
+
     /**
      * Run the database seeds.
      *
@@ -13,6 +26,9 @@ class JeuxSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $nom => $faker->name;
+         app/Models/jeu::factory(10)->create();
+
+
     }
 }
