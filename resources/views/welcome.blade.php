4d39e089934419101a208cf5bfb92655fc5ae4b7
Theo PRAT <theo@linux.home>
2020-12-17 13:35:20 +0100

    Jalon 3 css-welcome et css-regle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,24 @@
-<h1>Ma super page d'accueil</h1>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/welcome.css')}}">
+</head>
+<body>
+
+
+    <div class="texteMilieu">
+       <h1> LA MEDUSATHEQUE</h1>
+    </div>
+    <img id="page-profile" src="{{asset('images/photo-profil.jpg')}}" alt="ma photo de profil">
 
 
 
 @auth
-    <a href="{{Route('randomGames',5)}}"><button> 5 jeux aléatoire </button></a>
+    <a href="{{Route('randomGames',5)}}"><button class="button"> Nous vous proposons 5 jeux </br> aléatoire du moment </button></a>
 
 @endauth
+
+</body>
+</html>
